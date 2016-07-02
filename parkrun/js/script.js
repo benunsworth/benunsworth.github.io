e548c7750f5ff90784a92a68eb7c4a39bf99865c
Ben Unsworth <ben.unsworth1@gmail.com>
2016-07-02 12:06:02 +0100

    Add new parkrun
    Adding Roundshaw to Parkrun map

--- parkrun/js/script.js
+++ parkrun/js/script.js
@@ -30,4 +30,5 @@ L.marker([51.441168, -0.372075]).addTo(map).bindPopup("<b>Crane Park</b><br>PB: 
 L.marker([51.365295, 1.041942]).addTo(map).bindPopup("<b>Whitstable</b><br>PB: 20:04<br>Runs: 1");
 L.marker([51.247383, -0.562062]).addTo(map).bindPopup("<b>Guildford</b><br>PB: 22:13<br>Runs: 1");
 L.marker([51.364807, -0.079973]).addTo(map).bindPopup("<b>Lloyd</b><br>PB: 22:04<br>Runs: 1");
-L.marker([51.489403, -0.349889]).addTo(map).bindPopup("<b>Osterley</b><br>PB: 21:37<br>Runs: 1")
+L.marker([51.489403, -0.349889]).addTo(map).bindPopup("<b>Osterley</b><br>PB: 21:37<br>Runs: 1");
+L.marker([51.352253, -0.118720]).addTo(map).bindPopup("<b>Roundshaw Downs</b><br>PB: 26:07<br>Runs: 1")
