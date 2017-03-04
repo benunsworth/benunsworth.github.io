0f73cc9e152cf3c18d669ddf834f4a49cfe4baa5
Ben Unsworth <ben.unsworth1@gmail.com>
2017-03-04 11:55:50 +0000

    Adding new parkrun

--- parkrun/js/script.js
+++ parkrun/js/script.js
@@ -41,4 +41,5 @@ L.marker([51.311708, -0.556204]).addTo(map).bindPopup("<b>Woking</b><br>PB: 26:3
 L.marker([51.449459, 0.075155]).addTo(map).bindPopup("<b>Greenwich</b><br>PB: 21:37<br>Runs: 1");
 L.marker([51.540329, -0.547600]).addTo(map).bindPopup("<b>Black Park</b><br>PB: 20:57<br>Runs: 1");
 L.marker([51.558598, -0.168185]).addTo(map).bindPopup("<b>Hampstead Heath</b><br>PB: 23:38<br>Runs: 1");
-L.marker([51.366808, -0.539078]).addTo(map).bindPopup("<b>Homewood</b><br>PB: 22:59<br>Runs: 1")
+L.marker([51.366808, -0.539078]).addTo(map).bindPopup("<b>Homewood</b><br>PB: 22:59<br>Runs: 1");
+L.marker([51.417952, -0.006042]).addTo(map).bindPopup("<b>Beckenham Place</b><br>PB: 20:00<br>Runs: 1")
