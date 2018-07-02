9662fa64d59db43fd85f07fecb0fba1b7299e04f
Ben Unsworth <ben.unsworth1@gmail.com>
2018-07-02 12:31:49 +0100

    Add new run and update run count
    New run at Lullingstone and updating total runs at Dulwich

--- parkrun/js/script.js
+++ parkrun/js/script.js
@@ -14,7 +14,7 @@ L.marker([51.440762, -0.451523]).addTo(map).bindPopup("<b>Bedfont Lakes</b><br>P
 L.marker([51.455132, 0.113254]).addTo(map).bindPopup("<b>Bexley</b><br>PB: 24:34<br>Runs: 1");
 L.marker([51.433342, -0.146770]).addTo(map).bindPopup("<b>Tooting Common</b><br>PB: 19:25<br>Runs: 5");
 L.marker([51.472465, -0.218493]).addTo(map).bindPopup("<b>Fulham Palace</b><br>PB: 19:07<br>Runs: 1");
-L.marker([51.443869, -0.079206]).addTo(map).bindPopup("<b>Dulwich</b><br>PB: 19:37<br>Runs: 12");
+L.marker([51.443869, -0.079206]).addTo(map).bindPopup("<b>Dulwich</b><br>PB: 19:37<br>Runs: 13");
 L.marker([50.971433, -1.374032]).addTo(map).bindPopup("<b>Eastleigh</b><br>PB: 23:40<br>Runs: 1");
 L.marker([51.484536, -0.081024]).addTo(map).bindPopup("<b>Burgess</b><br>PB: 19:40<br>Runs: 3");
 L.marker([50.792835, -3.455007]).addTo(map).bindPopup("<b>Killerton</b><br>PB: 20:54<br>Runs: 1");
@@ -52,7 +52,8 @@ L.marker([51.298622, -0.724851]).addTo(map).bindPopup("<b>Frimley Lodge</><br>PB
 L.marker([51.234350, -0.208644]).addTo(map).bindPopup("<b>Reigate Priory</><br>PB: 30:35<br>Runs: 1");
 L.marker([51.097000, -0.175623]).addTo(map).bindPopup("<b>Tilgate</><br>PB: 30:38<br>Runs: 1");
 L.marker([50.810312, -0.364500]).addTo(map).bindPopup("<b>Worthing</><br>PB: 20:56<br>Runs: 1");
-L.marker([51.786634, -0.659599]).addTo(map).bindPopup("<b>Tring</><br>PB: 25:40<br>Runs: 1")
+L.marker([51.786634, -0.659599]).addTo(map).bindPopup("<b>Tring</><br>PB: 25:40<br>Runs: 1");
+L.marker([51.353118, 0.187147]).addTo(map).bindPopup("<b>Lullingstone</><br>PB: 26.43<Runs: 1")
 // for new rows L.marker([]).addTo(map).bindPopup("<b>event name</><br>PB: <br>Runs: 1")
 
 
